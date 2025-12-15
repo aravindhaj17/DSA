263f7e8db3c5f07ca4f19cc43044ffa3cca54c47
Aravindh A J <118287215+aravindhaj17@users.noreply.github.com>
2025-12-16 01:20:14 +0530

    Update and rename Valid Palindrome to Strings/ 09_Palindrome-Number.cpp

---
+++ Strings/ 09_Palindrome-Number.cpp
@@ -0,0 +1,22 @@
+// LeetCode 9: Palindrome Number
+// Difficulty: Easy
+// Approach: Reverse Half of Number
+// Time Complexity: O(log10(n))
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0))
+            return false;
+
+        int reversedHalf = 0;
+
+        while (x > reversedHalf) {
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x /= 10;
+        }
+
+        return (x == reversedHalf || x == reversedHalf / 10);
+    }
+};
