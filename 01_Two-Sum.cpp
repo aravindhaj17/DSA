15d89d961926611c9412b9a8dc8104e88819ae67
Aravindh A J <118287215+aravindhaj17@users.noreply.github.com>
2025-12-16 01:04:24 +0530

    Update and rename Two Sum to 01_Two-Sum.cpp

---
+++ 01_Two-Sum.cpp
@@ -0,0 +1,27 @@
+// LeetCode 1: Two Sum
+// Difficulty: Easy
+// Approach: Hash Map
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp; // value -> index
+
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+
+            if (mp.find(complement) != mp.end()) {
+                return { mp[complement], i };
+            }
+
+            mp[nums[i]] = i;
+        }
+        return {}; 
+    }
+};
