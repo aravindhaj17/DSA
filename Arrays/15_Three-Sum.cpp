6653237d07986586c81e7466b4c5b4c2c7b3a426
Aravindh A J <118287215+aravindhaj17@users.noreply.github.com>
2025-12-17 10:32:01 +0530

    Create 15_Three-Sum.cpp

---
+++ Arrays/15_Three-Sum.cpp
@@ -0,0 +1,51 @@
+// LeetCode 15: Three Sum
+// Difficulty: Medium
+// Approach: Sorting + Two Pointers
+// Time Complexity: O(n^2)
+// Space Complexity: O(1) (excluding output)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int n = nums.size();
+
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < n; i++) {
+
+            // Skip duplicate fixed elements
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    ans.push_back({nums[i], nums[left], nums[right]});
+                    left++;
+                    right--;
+
+                    // Skip duplicates
+                    while (left < right && nums[left] == nums[left - 1])
+                        left++;
+                    while (left < right && nums[right] == nums[right + 1])
+                        right--;
+                }
+                else if (sum < 0) {
+                    left++;
+                }
+                else {
+                    right--;
+                }
+            }
+        }
+        return ans;
+    }
+};
