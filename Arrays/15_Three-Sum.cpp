d895bb272520647402b83ab4d358b0c3b60e0c18
Aravindh A J <118287215+aravindhaj17@users.noreply.github.com>
2026-01-11 23:55:33 +0530

    Update 15_Three-Sum.cpp

--- Arrays/15_Three-Sum.cpp
+++ Arrays/15_Three-Sum.cpp
@@ -1,5 +1,5 @@
-// LeetCode 15: Three Sum.
-// Difficulty: Medium.
+// LeetCode 15: Three Sum
+// Difficulty: Medium
 // Approach: Sorting + Two Pointers
 // Time Complexity: O(n^2)
 // Space Complexity: O(1) (excluding output)
